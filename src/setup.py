530bd703a801d3dbefc7c90fb07f1582ca441c26
Michael Ottoson <michael@pointw.com>
2021-02-07 10:46:02 -0500

    added --as_parent_ref to mkrel

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ bin = glob('bin/*')
 
 setup(
     name='eve-utils',
-    version='0.6.7',
+    version='0.6.8',
     description='Templates and scripts to rapidly spin up an Eve-based API.',
     long_description=open('README.rst').read(),
     license='MIT',
