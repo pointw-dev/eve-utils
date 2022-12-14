1e01d68a4d6ae7eef1be7973a4b28efba8b9404c
Michael Ottoson <michael@pointw.com>
2022-12-13 23:00:45 -0500

    version -t will move a tag if it exists

--- src/version.py
+++ src/version.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import os
+import platform
 import argparse
 from datetime import date
 
@@ -15,30 +16,35 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--set', help='Provide the new version to set.')
     parser.add_argument('-t', '--tag', help='Create git tag with current version (ignored if used with --set).', action='store_true')
-    
-    
+
+
     args = parser.parse_args()
-    
+
     current_version = VERSION
-    
+
     if args.set:
         new_version = args.set
         print(f'Changing to {new_version}')
-        
+
         with open('version.py', 'r') as f:
             lines = f.readlines()
-            
+
         with open('version.py', 'w') as f:
             for line in lines:
                 if line.startswith("VERSION = '"):
                     line = f"VERSION = '{new_version}'\n"
                 f.write(line)
-                
+
         current_version = new_version
     elif args.tag:  # cannot tag when file is freshly changed - must add/commit first
         print(f'creating git tag {current_version}')
         today = date.today()
+        silent = ' > /dev/null 2> /dev/null'
+        if platform.system() == 'Windows':
+            silent = ' > nul 2> nul'
+
         comment = f'Released {today}'
+        os.system(f'git tag -d {current_version} {silent}')
         os.system(f'git tag -a {current_version} -m "{comment}"')
 
 
